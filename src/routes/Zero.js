0dc6e386c51d0e96da74e190c1c1ebcb17cf419b
Elwerco <alexsteev2002@gmail.com>
2018-07-29 23:36:18 +0300

    aseferf

--- src/routes/Zero.js
+++ src/routes/Zero.js
@@ -4,6 +4,8 @@ import { Icon, Container, Button, Item } from 'semantic-ui-react';
 import Menu from '../containers/Menu';
 import axios from 'axios';
 import { Redirect } from 'react-router';
+import  { connect } from 'react-redux';
+
 
 
 
@@ -25,9 +27,11 @@ class Zero extends Component {
       this.setState({isRedirect: true});
     }
 
+    state() {
 
-    render() {
+    }
 
+    render(state) {
       const { match: { params } } = this.props;
       // Эта штука сверху берет айдишник. А в Zero найешь остальное(для себя)
       // const a = axios.get('/books.json')
@@ -45,7 +49,6 @@ class Zero extends Component {
               return <Redirect to='/' />
           } 
 
-
       return(
         <div>
           <Container>
@@ -53,7 +56,6 @@ class Zero extends Component {
             <Item.Group relaxed>
               <Item>
                 <Item.Image size='large' src={image} />
-
                 <Item.Content verticalAlign='top'>
                   <Item.Header style={style.second}>{title}</Item.Header>
                   <Item.Description style={style.first}>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</Item.Description>
@@ -77,4 +79,9 @@ class Zero extends Component {
     }
 }
 
-export default Zero;
+
+          const mapStateToProps = (state) => {
+          return alert(state);
+         };
+
+export default connect(mapStateToProps)(Zero);
